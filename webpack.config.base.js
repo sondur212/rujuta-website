e6dec5ee22938c1864196dd9eeee29fdb69ac11c
Raghavendra Sondur <rmsondur@yahoo.com>
2020-08-16 23:20:19 +0530

    add file-loader for images

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -35,6 +35,10 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   plugins: [
