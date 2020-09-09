71d1eb919993cf11f82014bb9e19e71a927e398c
Admin <42864@DawningSystems-MacBook-Pro.local>
2020-09-09 21:11:25 +0530

    add resize for resume view

--- src/views/resume/resume-view.js
+++ src/views/resume/resume-view.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import throttle from 'lodash/throttle';
 import { Document, Page } from 'react-pdf';
 import './resume-view.scss';
 import resume from './../../../assets/images/resumefinal.pdf';
 
 const ResumeView = () => {
+  const pageRef = useRef(null);
+  const [initialWidth, setInitialWidth] = useState(null);
+  const setPdfSize = () => {
+    if (pageRef && pageRef.current) {
+      setInitialWidth(pageRef.current.getBoundingClientRect().width);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', throttle(setPdfSize, 1000));
+    setPdfSize();
+    return () => {
+      window.removeEventListener('resize', throttle(setPdfSize, 1000));
+    };
+  }, []);
+  const resumeWidth = Math.floor(initialWidth * 0.45);
   return (
-    <div className="resume-view">
-      {/* <embed src={resume} width="800px" height="1200px" /> */}
+    <div ref={pageRef} className="resume-view">
       <Document file={resume}>
-        <Page pageNumber={1} />
+        <Page width={resumeWidth} pageNumber={1} />
       </Document>
     </div>
   );
